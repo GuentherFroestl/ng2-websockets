8ed2290dc57650fce7e530619a2b951a81ffb864
Günther Fröstl <gfr@gammadata.de>
2016-09-21 13:40:32 +0200

    - clean up

--- client/app/shared/services/chat.service2.ts
+++ client/app/shared/services/chat.service2.ts
@@ -1,18 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Rx';
-//import {WebSocketService } from './websocket.service';
-
 import {$WebSocket} from 'angular2-websocket/angular2-websocket'
-
-//const CHAT_URL = 'ws://172.27.15.128:8080/softwarecatalogmgmt-softwarespec/wx/v1/notifier';
-//const CHAT_URL = 'ws://localhost:3005';
-
-const CHAT_URL = 'ws://sapp-dev-02:8080/softwarecatalogmgmt-softwarespec/wx/v1/notifier';
-
-/*
- * ws://172.27.15.128:8080/softwarecatalogmgmt-softwarespec/wx/v1/notifier 
-
- */
 
 
 @Injectable()
